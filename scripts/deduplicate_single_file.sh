541d7572a1f5714783a603aafb7e201ace811022
Sarah Yurick <53962159+sarahyurick@users.noreply.github.com>
2025-03-03 15:26:02 -0800

    Update deduplicate_single_file.sh

--- scripts/deduplicate_single_file.sh
+++ scripts/deduplicate_single_file.sh
@@ -1,5 +1,29 @@
-set -e
-python3 scripts/make_suffix_array.py $1
-cargo run self-similar --data-file $1 --length-threshold $3 --cache-dir /tmp/cache --num-threads $4
-cargo run collect --data-file $1 --cache-dir /tmp/cache --length-threshold $3 > /tmp/drop_tokens_file
-python3 scripts/finish_single_file.py $1 /tmp/drop_tokens_file $2
+INPUT_FILE=/path/to/input/file.jsonl
+THRESHOLD=50
+CACHE=/path/to/cache/
+DROP_TOKENS_FILE=/path/to/drop_tokens_file
+DEDUPED_FILE=/path/to/output/file_deduped.jsonl
+
+start_time=$(date +%s)
+python3 scripts/make_suffix_array.py $INPUT_FILE
+end_time=$(date +%s)
+elapsed=$((end_time - start_time))
+echo "Time taken to make suffix array: $elapsed seconds"
+
+start_time=$(date +%s)
+cargo run self-similar --data-file $INPUT_FILE --length-threshold $THRESHOLD --cache-dir $CACHE
+end_time=$(date +%s)
+elapsed=$((end_time - start_time))
+echo "Time taken to run self-similar: $elapsed seconds"
+
+start_time=$(date +%s)
+cargo run collect --data-file $INPUT_FILE --cache-dir $CACHE --length-threshold $THRESHOLD > $DROP_TOKENS_FILE
+end_time=$(date +%s)
+elapsed=$((end_time - start_time))
+echo "Time taken to run collect: $elapsed seconds"
+
+start_time=$(date +%s)
+python3 scripts/finish_single_file.py $INPUT_FILE $DROP_TOKENS_FILE $DEDUPED_FILE
+end_time=$(date +%s)
+elapsed=$((end_time - start_time))
+echo "Time taken to finish single file: $elapsed seconds"
